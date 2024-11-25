46b4b45112fb024e53e4fe6e2e11b8129b9fa5ca
Simon <simon@yard.nl>
2024-11-25 15:35:28 +0100

    (chore): apply php-cs-fixer rules

--- src/SearchWP.php
+++ src/SearchWP.php
@@ -8,26 +8,26 @@ use Yard\Hook\Filter;
 
 class SearchWP
 {
-    #[Filter('searchwp\native\short_circuit')]
-    public function shortCircuit(): bool
-    {
-        return true;
-    }
+	#[Filter('searchwp\native\short_circuit')]
+	public function shortCircuit(): bool
+	{
+		return true;
+	}
 
-    #[Filter('searchwp\rest')]
-    public function disableRest(): bool
-    {
-        return false;
-    }
+	#[Filter('searchwp\rest')]
+	public function disableRest(): bool
+	{
+		return false;
+	}
 
-    #[Filter('searchwp\swp_query\args')]
-    public function changePostsPerPage(array $args): array
-    {
-        // Change FacetWP default maximum of 200 results to unlimited
-        if (isset($args['facetwp'])) {
-            $args['posts_per_page'] = -1;
-        }
+	#[Filter('searchwp\swp_query\args')]
+	public function changePostsPerPage(array $args): array
+	{
+		// Change FacetWP default maximum of 200 results to unlimited
+		if (isset($args['facetwp'])) {
+			$args['posts_per_page'] = -1;
+		}
 
-        return $args;
-    }
+		return $args;
+	}
 }
