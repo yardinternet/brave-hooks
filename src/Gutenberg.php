9c7a7099cb3be644af6e527984ca6ffdd727ccdd
Simon <simon@yard.nl>
2024-11-25 15:59:32 +0100

    (fix): yard::gutenberg/allowed-core-blocks

--- src/Gutenberg.php
+++ src/Gutenberg.php
@@ -8,22 +8,23 @@ use Yard\Hook\Filter;
 
 class Gutenberg
 {
-    #[Filter('yard::gutenberg/allowed-blocks')]
-    public function registerYardGutenbergBlocks(): array
-    {
-        return config('gutenberg.allowedBlocks', []);
-    }
+	#[Filter('yard::gutenberg/allowed-blocks')]
+	public function registerYardGutenbergBlocks(): array
+	{
+		return config('gutenberg.allowedBlocks', []);
+	}
 
-    #[Filter('yard::gutenberg/allowed-core-blocks')]
-    public function registerCoreBlocks(array $initialAllowedBlocks): array
-    {
-        $additionalBlocks = collect(config('gutenberg.allowedCoreBlocks', []));
+	#[Filter('yard::gutenberg/allowed-core-blocks')]
+	public function registerCoreBlocks(array $initialAllowedBlocks): array
+	{
+		$additionalBlocks = collect(config('gutenberg.allowedCoreBlocks', []));
 
-        $excludeBlocks = collect(config('gutenberg.excludedCoreBlocks', []));
+		$excludeBlocks = collect(config('gutenberg.excludedCoreBlocks', []));
 
-        return collect($initialAllowedBlocks)
-            ->merge($additionalBlocks)
-            ->reject(fn ($block) => $excludeBlocks->contains($block))
-            ->all();
-    }
+		return collect($initialAllowedBlocks)
+			->merge($additionalBlocks)
+			->reject(fn ($block) => $excludeBlocks->contains($block))
+			->values()
+			->all();
+	}
 }
