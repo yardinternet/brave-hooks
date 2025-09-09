68c519c6b8795543d2bcb20e416007f77ff22e6d
Maarten Bruna <14947039+ictbeheer@users.noreply.github.com>
2025-09-09 14:10:07 +0200

    (fix): delete network user

--- src/User.php
+++ src/User.php
@@ -4,18 +4,35 @@ declare(strict_types=1);
 
 namespace Yard\Brave\Hooks;
 
+use Yard\Hook\Action;
 use Yard\Hook\Filter;
 
 class User
 {
-	#[Filter('deleted_user')]
+	public const DELETE_NETWORK_USER_CRON_HOOK = 'delete_network_user';
+
+	#[Action('remove_user_from_blog')]
+	public function scheduleDeleteNetworkUser(int $userID): void
+	{
+		\wp_schedule_single_event(time() + 5, self::DELETE_NETWORK_USER_CRON_HOOK, [$userID]);
+	}
+
+	#[Filter(self::DELETE_NETWORK_USER_CRON_HOOK)]
 	public function deleteNetworkUser(int $id): void
 	{
+		if (get_user($id) === false) {
+			return;
+		}
+
 		if (count(get_blogs_of_user($id)) > 0) {
 			return;
 		}
 
-		wpmu_delete_user($id);
+		if (! function_exists('wpmu_delete_user')) {
+			require_once ABSPATH . '/wp-admin/includes/ms.php';
+		}
+
+		\wpmu_delete_user($id);
 	}
 
 	#[Filter('network_site_url')]
