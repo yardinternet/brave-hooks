37029a4b30579653c110d59d3fbae56db1d2bb57
Roric van Schalkwijk <roric.vanschalkwijk@yard.nl>
2026-06-16 09:40:14 +0200

    feat: simple-history hooks

--- config/hooks.php
+++ config/hooks.php
@@ -17,6 +17,7 @@ return [
 	'searchwp' => Yard\Brave\Hooks\SearchWP::class,
 	'security' => Yard\Brave\Hooks\Security::class,
 	'seopress' => Yard\Brave\Hooks\SEOPress::class,
+	'simple-history' => Yard\Brave\Hooks\SimpleHistory::class,
 	'user' => Yard\Brave\Hooks\User::class,
 	'warden' => Yard\Brave\Hooks\Warden::class,
 ];
